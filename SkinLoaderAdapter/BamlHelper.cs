ef94f2e45000f798a63b6624c28089515668e992
Mattias Cibien <mattias@mattiascibien.net>
2014-10-06 13:07:17 +0200

    Added WPF SkinLoader from Codeproject  - http://www.codeproject.com/Articles/22575/WPF-Themes-and-Skins-Engine

---
+++ SkinLoaderAdapter/BamlHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using System.Windows.Baml2006;
+using System.Windows.Markup;
+
+namespace Tomers.WPF.Themes
+{
+	public static class BamlHelper
+	{
+		public static TRoot LoadBaml<TRoot>(Stream stream)
+		{
+            Baml2006Reader baml = new Baml2006Reader(stream);
+            object bamlRoot = XamlReader.Load(baml);
+			return (TRoot)bamlRoot;
+		}
+	}
+}
---
+++ SkinLoaderAdapter/README.md
@@ -0,0 +1,6 @@
+Original Work: [http://www.codeproject.com/Articles/22575/WPF-Themes-and-Skins-Engine](http://www.codeproject.com/Articles/22575/WPF-Themes-and-Skins-Engine)
+
+Added features
+ 
+ * Support for .NET 4.5.1
+ * No more reflection for loading BAML.
