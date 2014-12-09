56b8f2cbd1a396800bfd6229fcf8e1f311ef8f42
Mattias Cibien <mattias@mattiascibien.net>
2014-12-09 16:01:44 +0100

    Added stubs for loading and saving layout data.

---
+++ SuperbEdit/Layout/LayoutInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Wpf.AvalonDock.Layout;
+
+namespace SuperbEdit.Layout
+{
+    /// <summary>
+    /// Class for providing layout initialization 
+    /// </summary>
+    internal class LayoutInitializer : ILayoutUpdateStrategy
+    {
+        public void AfterInsertAnchorable(LayoutRoot layout, LayoutAnchorable anchorableShown)
+        {
+            
+        }
+
+        public void AfterInsertDocument(LayoutRoot layout, LayoutDocument anchorableShown)
+        {
+           
+        }
+
+        public bool BeforeInsertAnchorable(LayoutRoot layout, LayoutAnchorable anchorableToShow, ILayoutContainer destinationContainer)
+        {
+            return true;
+        }
+
+        public bool BeforeInsertDocument(LayoutRoot layout, LayoutDocument anchorableToShow, ILayoutContainer destinationContainer)
+        {
+            return true;
+        }
+    }
+}
