a21a37b57a2af9d98abafe8facfe95dba5fce7d3
Mattias Cibien <mattias@mattiascibien.net>
2014-12-11 17:11:51 +0100

    Continued the stub for saving and restoring panel positions.

--- SuperbEdit/Layout/LayoutInitializer.cs
+++ SuperbEdit/Layout/LayoutInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SuperbEdit.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,22 +15,35 @@ namespace SuperbEdit.Layout
     {
         public void AfterInsertAnchorable(LayoutRoot layout, LayoutAnchorable anchorableShown)
         {
-            
+
         }
 
         public void AfterInsertDocument(LayoutRoot layout, LayoutDocument anchorableShown)
         {
-           
+
         }
 
         public bool BeforeInsertAnchorable(LayoutRoot layout, LayoutAnchorable anchorableToShow, ILayoutContainer destinationContainer)
         {
-            return true;
+            //TODO: add logic for setting the right panel position
+            switch ((anchorableToShow.Content as Panel).DefaultPosition)
+            {
+                case PanelPosition.Left:
+                    break;
+                case PanelPosition.Right:
+                    break;
+                case PanelPosition.Bottom:
+                    break;
+                default:
+                    break;
+            }
+
+            return false;
         }
 
         public bool BeforeInsertDocument(LayoutRoot layout, LayoutDocument anchorableToShow, ILayoutContainer destinationContainer)
         {
-            return true;
+            return false;
         }
     }
 }
---
+++ SuperbEdit/Layout/LoadSaveLayoutBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using SuperbEdit.Base;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using Xceed.Wpf.AvalonDock;
+using Xceed.Wpf.AvalonDock.Layout.Serialization;
+
+namespace SuperbEdit.Layout
+{
+    public class LoadSaveLayoutBehaviour : Behavior<DockingManager>
+    {
+        readonly string filePath = Path.Combine(Folders.UserFolder, "layout.xml");
+
+        XmlLayoutSerializer serializer;
+
+        protected override void OnAttached()
+        {
+            serializer = new XmlLayoutSerializer(AssociatedObject);
+            Window theWindow = Window.GetWindow(AssociatedObject);
+
+            if (File.Exists(filePath))
+                serializer.Deserialize(filePath);
+
+            theWindow.Closed += (sender, e) =>
+            {
+                serializer.Serialize(filePath);
+            };
+
+            base.OnAttached();
+        }
+    }
+}
