cf288d610862403466526cb0a091d92696bdaeb1
Mattias Cibien <mattias@mattiascibien.net>
2015-04-14 11:58:06 +0200

    Retrieve contributors from Github.

--- SuperbEdit/ViewModels/AboutViewModel.cs
+++ SuperbEdit/ViewModels/AboutViewModel.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Security.Policy;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using Caliburn.Micro;
+using Octokit;
 using SuperbEdit.Base;
 using SuperbEdit.Base.Scripting;
 
@@ -16,6 +22,12 @@ namespace SuperbEdit.ViewModels
         public string Author { get; set; }
     }
 
+    public class ContributorItem
+    {
+        public string Name { get; set; }
+        public string GitHubUrl { get; set; }
+        public ImageSource Photo { get; set; }
+    }
 
     [Export]
     public sealed class AboutViewModel : Screen
@@ -23,6 +35,7 @@ namespace SuperbEdit.ViewModels
         public AboutViewModel()
         {
             DisplayName = "About SuperbEdit";
+            Contributors = new ObservableCollection<ContributorItem>();
             LoadedPackages =
                 AssemblyListComposer.loadedAssemblies
                     .Select(ass => new PackageItem
@@ -41,6 +54,39 @@ namespace SuperbEdit.ViewModels
                         //TODO: implement a way to diplay authorship information for scripted plugins
                         Author = "Compiled From Script"
                     }));
+        }
+
+        protected override async void OnActivate()
+        {
+            try
+            {
+                var github = new GitHubClient(new ProductHeaderValue("SuperbEdit"));
+                var contributors = await github.Repository.GetAllContributors("SuperbEdit", "SuperbEdit", true);
+                Contributors.Clear();
+
+                foreach (var contributor in contributors)
+                {
+                    var githubUser = await github.User.Get(contributor.Login);
+
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(githubUser.AvatarUrl, UriKind.Absolute);
+                    bitmap.EndInit();
+
+                    Contributors.Add(new ContributorItem()
+                    {
+                        GitHubUrl = contributor.HtmlUrl,
+                        Name = githubUser.Name,
+                        Photo = bitmap
+                    });
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            base.OnActivate();
         }
 
         public string Version
@@ -72,6 +118,11 @@ namespace SuperbEdit.ViewModels
                 }
                 return ret;
             }
+        }
+
+        public ObservableCollection<ContributorItem> Contributors
+        {
+            get; private set;
         }
 
         public string Copyright
