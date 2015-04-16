57cff2859120dea40aa25e28adf68ac2bbf4380d
Mattias Cibien <mattias@mattiascibien.net>
2015-04-16 09:46:06 +0200

    Began find and replace dialog.

---
+++ SuperbEdit/Actions/EditMenu/FindAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using SuperbEdit.Base;
+using SuperbEdit.ViewModels;
+
+namespace SuperbEdit.Actions
+{
+    [ExportAction(Menu = "Edit", Order = 6, Owner = "Shell", RegisterInCommandWindow = true)]
+    public class FindAction : ActionItem
+    {
+        [Import]
+        private IWindowManager _windowManager;
+
+        public FindAction()
+            :base("Find...", "Opens the find and replace dialog...", "Edit.Find")
+        { }
+
+        public override void Execute()
+        {
+            //TODO: should not create one view every time 
+            _windowManager.ShowDialog(new FindReplaceViewModel());
+        }
+    }
+}
---
+++ SuperbEdit/ViewModels/FindReplaceViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using SuperbEdit.Base;
+
+namespace SuperbEdit.ViewModels
+{
+    public class FindReplaceViewModel : Screen
+    {
+        [Import] 
+        private Lazy<IShell> _shell; 
+
+        public FindReplaceViewModel()
+        {
+            DisplayName = "Find & Replace";
+        }
+    }
+}
